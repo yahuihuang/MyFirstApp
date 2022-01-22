0db1db88af28f84925b8fc244e9b700511dcf912
Huang Grace <huang.yahui.grace@gmail.com>
2022-01-22 22:35:39 +0800

    init

--- Test20220121/ViewController.swift
+++ Test20220121/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblText: UILabel!
+    @IBAction func onClick(_ sender: Any) {
+        print("test: \(String(describing: lblText.text!))")
+        var str = lblText.text!
+        print("\(str)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
